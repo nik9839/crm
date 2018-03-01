870f07050635d113ff8e05c8b38558cb83998354
Nikhil <nikhilgupta709@gmail.com>
2018-03-01 12:57:07 +0530

    uncomment databases config - production

--- crm/production.py
+++ crm/production.py
@@ -87,13 +87,12 @@ CACHES ={
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-'default': {
-            #'PORT': '',
-        #'ENGINE': 'django.db.backends.postgresql',
-        #'NAME': os.getenv('DB_NAME', ''),
-        #'USER': os.getenv('DB_USER', ''),
-        #'PASSWORD': os.getenv('DB_PASSWORD', ''),
-        #'HOST': os.getenv('DB_HOST', ''),
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', ''),
+        'USER': os.getenv('DB_USER', ''),
+        'PASSWORD': os.getenv('DB_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', ''),
     }
 }
 
