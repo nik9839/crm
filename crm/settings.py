45eb7c296fb5343189c5d23adfa1b7f0503a5242
Nikhil <nikhilgupta709@gmail.com>
2018-03-05 18:08:28 +0530

    added django-recurrence app

--- crm/settings.py
+++ crm/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'recurrence',
     'rest_framework',
     'MyResources.apps.MyresourcesConfig',
     'django.contrib.admin',
